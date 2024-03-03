6a808c866d22db5f25f55cc577f9a73e998029b3
pglez82 <pglez82@gmail.com>
2024-03-03 19:37:36 +0100

    fixing test

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the 2024 edition of the Software Architecture course/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText(/Welcome to the 2024 edition of the Software Architecture course/i);
+  expect(welcomeMessage).toBeInTheDocument();
 });
+
+
